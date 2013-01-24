708f39890f3cfcdde5fe7b6adfe4e8327cb31afa
John Mei <jqm.usa@gmail.com>
2013-01-24 09:32:58 +0800

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -20,4 +20,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	assert_response :success
   end
 
+  test "that a profile page works" do
+    get '/jqm'
+    assert_response :success
+  end
 end

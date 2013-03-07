6c025399c747f2d95f5fa43a7df6904afab178b5
John Mei <jqm.usa@gmail.com>
2013-03-08 01:47:01 +0800

    Scoped status updates to the current user on create and update.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -31,11 +31,23 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should create status when logged in" do
     sign_in users(:yuwei)
+    
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
 
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:yuwei)
+    
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:yuwei2).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:yuwei).id
   end
 
   test "should show status" do
@@ -65,6 +77,20 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:yuwei)
     put :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in " do
+    sign_in users(:yuwei)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:yuwei2).id}
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:yuwei).id
+  end
+
+  test "should not update the status if nothing has changed " do
+    sign_in users(:yuwei)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:yuwei).id
   end
 
   test "should destroy status" do

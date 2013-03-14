183b09ede1109d4f6cf272401f53e848d78d106d
John Mei <jqm.usa@gmail.com>
2013-03-14 13:33:43 +0800

    Add user friendships and associated tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
   	user = User.new
   	assert !user.save
@@ -45,4 +48,15 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
   end
 
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:yuwei).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:yuwei).friends << users(:mike)
+    users(:yuwei).friends.reload
+    assert users(:yuwei).friends.include?(users(:mike))
+  end
 end
